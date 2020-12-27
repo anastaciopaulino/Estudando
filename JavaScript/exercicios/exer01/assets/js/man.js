1ebc990ac3568216dacc36a17b864cb35b1e91a5
Anastácio Paulino <anastaciopaulino008@gmail.com>
2020-12-27 18:51:20 +0100

    Terminei o exercicio

--- JavaScript/exercicios/exer01/assets/js/man.js
+++ JavaScript/exercicios/exer01/assets/js/man.js
@@ -1,27 +1,39 @@
 
-
 function calcular() {
   let peso = document.getElementById('peso');
   let altura = document.getElementById('altura');
   let resultado = document.getElementById('resultado');
 
+  if(peso.value == "") {
+    resultado.innerHTML = "[!] Peso invalido [!]"
+    resultado.className = 'invalid'
+    return false
+  } else if(altura.value == "") {
+    resultado.innerHTML = "[!] Altura invalida [!]"
+    resultado.className = 'invalid'
+    return false
+  }
+
   // Calculado o IMC
-  let imc = peso / (altura**2);
+  let imc = parseFloat(peso.value) / parseFloat(altura.value)**2;
 
   let status;
 
   if (imc < 18.5) {
     status = 'Abaixo do Peso'
-  } else if (imc <= 18.5 && imc >= 24.9) {
+  } else if (imc >= 18.5 && imc <= 24.9) {
     status = 'Peso Normal'
-  } else if (imc <= 25 && imc >= 29.9){
+  } else if (imc >= 25 && imc <= 29.9){
     status = 'Sobrepeso'
-  } else if (imc <= 30 && imc >= 34.9){
+  } else if (imc >= 30 && imc <= 34.9){
     status = 'Obesidade grau 1'
-  } else if(imc <= 35 && imc >= 39.9) {
+  } else if(imc >= 35 && imc <= 39.9) {
     status = 'Obesidade grau 2'
   } else if(imc > 40) {
     status = 'Obesidade grau 3'
   }
+
+  resultado.innerHTML = 'O seu IMC é ' + imc  + " (" + status + ")"
+  resultado.className = 'success'
    
 }
