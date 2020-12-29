8f4d88e9998c3c5fa632cd9ba272ef94859dd14a
Anastácio Paulino <anastaciopaulino008@gmail.com>
2020-12-29 14:52:47 +0100

    Terminei a aula 11

---
+++ JavaScript/aula11/index.js
@@ -0,0 +1,22 @@
+/*
+FOR classico
+for (variavel de controlo; condição; incremento/decremento){
+  bloco de código
+}
+*/
+
+// console.log('Linha 1');
+// console.log('Linha 2');
+// console.log('Linha 3');
+// console.log('Linha 4');
+// console.log('Linha 5');
+// console.log('Linha 6');
+// console.log('Linha 7');
+
+const frutas = ['Maçã', 'Banana', 'Uva', 'Pêra', 'Manga', 'Melão', 'Mêlacia'];
+
+for (let i = 0; i < frutas.length; i += 1) {
+
+  console.log('Indice ',i , ' ', frutas[i])
+
+ }
