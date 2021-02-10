379945b9584dee28375ddd434c7602e4e3e07783
Anastácio Paulino <anastaciopaulino008@gmail.com>
2021-02-10 10:35:56 +0100

    Finalizei a API Todo (Django Rest Framework)

--- Python/Django/Django Rest Framework/Todo/backend/todo_project/todos/tests.py
+++ Python/Django/Django Rest Framework/Todo/backend/todo_project/todos/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from todos.models import Todo
 
 # Create your tests here.
+
+
+class TodoModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        Todo.objects.create(title='First todo', body='Body here')
+    
+    def test_title_content(self):
+        todo = Todo.objects.get(id=1)
+
+        expected_object_name = f'{todo.title}'
+        self.assertEquals(expected_object_name, 'First todo')
+
+    def test_body_content(self):
+        todo = Todo.objects.get(id=1)
+
+        expected_object_name = f'{todo.body}'
+        self.assertEquals(expected_object_name, 'Body here')
