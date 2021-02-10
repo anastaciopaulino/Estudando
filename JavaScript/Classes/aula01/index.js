228d10f1bf641188d7c667967b828eaafa2eb520
Anastácio Paulino <anastaciopaulino008@gmail.com>
2021-02-10 11:02:17 +0100

    Comecei o modulo sobre Classes(finalizei a aula 01)

---
+++ JavaScript/Classes/aula01/index.js
@@ -0,0 +1,17 @@
+class Pessoa {
+  constructor(nome, sobrenome) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+  }
+
+  falar() {
+    console.log(`${this.nome} está falando`);
+  }
+
+  comer() {
+    console.log(`${this.nome} está comendo`);
+  }
+}
+
+const p1 = new Pessoa('Anastácio', 'Paulino');
+console.log(p1);
