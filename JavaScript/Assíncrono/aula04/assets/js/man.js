5ae4c5efe6361325e888ab7825f900ad76fb6231
Anastacio Paulino <anastaciopaulino008@gmail.com>
2021-03-22 14:39:35 +0100

    Terminei as aulas sobre AJAX

---
+++ JavaScript/Assíncrono/aula04/assets/js/man.js
@@ -0,0 +1,50 @@
+/**
+ * Ajax (Ansicrony JavaScript XML)
+*/
+
+const request = (obj) => {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(obj.method, obj.url, true);
+    xhr.send();
+
+    xhr.addEventListener('load', () => {
+      if(xhr.status >= 200 && xhr.status < 300) {
+        resolve(xhr.responseText);
+      } else {
+        reject(xhr.statusText);
+      }
+    })
+  })
+  
+}
+
+document.addEventListener('click', e => {
+  const el = e.target;
+  const tag = el.tagName.toLowerCase();
+
+  if(tag === 'a') {
+    e.preventDefault();
+    carregaPagina(el);
+  }
+})
+
+async function carregaPagina(el){
+  const href = el.getAttribute('href');
+  const objGet = {
+    method: 'GET',
+    url: href
+  };
+
+  try {
+    const resultado = await request(objGet);
+    carregarResultado(resultado);  
+  } catch(e){
+    console.log(e);
+  }
+}
+
+function carregarResultado(response) {
+  const resultado = document.querySelector('.resultado');
+  resultado.innerHTML = response;
+}
