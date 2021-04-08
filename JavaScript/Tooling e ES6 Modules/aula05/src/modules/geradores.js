890536317e9afc0678472f02ff820487277d6eea
Anastacio Paulino <anastaciopaulino008@gmail.com>
2021-04-08 02:28:56 +0100

    Terminei o gerador de Senhas.

---
+++ JavaScript/Tooling e ES6 Modules/aula05/src/modules/geradores.js
@@ -0,0 +1,24 @@
+function rand(min, max){
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+const geraMaiuscula = () => String.fromCharCode(rand(65, 91));
+const geraMinuscula = () => String.fromCharCode(rand(97, 123));
+const geraNumero = () => String.fromCharCode(rand(48, 58));
+
+const simbolos = ',.;:><~´łªº§}][{¬¢£³²¹?ßæ«»©ð°?';
+const geraSimbolo = () => simbolos[rand(0, simbolos.length)];
+
+
+export default function geraSenha(qtd, maisculas, minusculas, numeros, simbolos) {
+  const senhaArray = [];
+  
+  for(let i = 0; i < Number(qtd); i++){
+    maisculas && senhaArray.push(geraMaiuscula());
+    minusculas && senhaArray.push(geraMinuscula());
+    numeros && senhaArray.push(geraNumero());
+    simbolos && senhaArray.push(geraSimbolo());
+  }
+
+  return senhaArray.join('').slice(0, qtd);
+}
